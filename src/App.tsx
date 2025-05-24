5e79e880ab8609325d7892bc5ac09f7289a4b663
hassohamid <hassohamid@hotmail.com>
2025-05-24 08:51:24 +0200

    Setting up structure

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,20 @@
-import { Button } from "./components/ui/button";
+import { Routes, Route } from "react-router-dom";
+import Dashboard from "./layout/Dashboard";
+import AccountPage from "./layout/AccountPage";
+import HomePage from "./layout/HomePage";
+import LoginPage from "./layout/LoginPage";
 
 function App() {
-  return <Button> Click me </Button>;
+  return (
+    <>
+      <Routes>
+        <Route path="/" element={<HomePage />} />{" "}
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/account" element={<AccountPage />} />
+      </Routes>
+    </>
+  );
 }
 
 export default App;
